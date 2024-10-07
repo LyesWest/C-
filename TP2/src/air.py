0cf2ea81e2b6711c642b9dc2bf1cea54ea969d48
LyesWest <lyes.mesbahi1@gmail.com>
2024-10-07 22:24:54 +0200

    Update air.py

--- TP2/src/air.py
+++ TP2/src/air.py
@@ -1,13 +1,11 @@
 import math
 
-# Fonction pour calculer l'aire d'un cercle
 def calculer_aire(rayon: float) -> float:
-    return math.pi * rayon * rayon  # Utilise la constante pi et calcule l'aire
+    return math.pi * rayon * rayon
 
-# Fonction principale pour tester la fonction calculer_aire
 def main() -> None:
-    rayon: float = 5.0  # Rayon du cercle
-    aire: float = calculer_aire(rayon)  # Calcul de l'aire
+    rayon : float = 5.0
+    aire : float = calculer_aire(rayon)
     print(f"L'aire du cercle de rayon {rayon:.2f} est {aire:.2f}")
 
 if __name__ == "__main__":
