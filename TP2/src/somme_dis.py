fd534629874a86572eb40495d5e7ede762696e7b
LyesWest <lyes.mesbahi1@gmail.com>
2024-10-07 22:28:18 +0200

    Update somme_dis.py

--- TP2/src/somme_dis.py
+++ TP2/src/somme_dis.py
@@ -1,12 +1,19 @@
 import dis
 
-# Fonction pour calculer la somme des entiers de 0 à n-1
-def calculer_somme(n: int) -> int:
+"""def calculer_somme(n: int) -> int:
     somme = 0
-    # Boucle for pour parcourir tous les entiers de 0 à n-1
     for i in range(n):
         somme += i
-    return somme
+    return somme 
+"""
+
+def calculer_somme(n: int) -> int:
+       somme = 0
+       i = 0
+       while i < n:
+           somme += i
+           i += 1
+       return somme
 
 def main() -> None:
     n = 10
@@ -17,17 +24,3 @@ if __name__ == "__main__":
     # Utilisation de dis pour afficher le bytecode de la fonction
     dis.dis(calculer_somme)
     main()
-
-# Version modifiée de `calculer_somme` avec une boucle while
-def calculer_somme_while(n: int) -> int:
-    somme = 0
-    i = 0
-    # Boucle while pour parcourir les entiers de 0 à n-1
-    while i < n:
-        somme += i
-        i += 1
-    return somme
-
-# Analyse du bytecode de la version modifiée
-dis.dis(calculer_somme_while)
-
