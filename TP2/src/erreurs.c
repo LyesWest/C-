e90c016a0c8663fee364acae85e0763e6aba6f2a
LyesWest <lyes.mesbahi1@gmail.com>
2024-10-07 22:26:15 +0200

    Update erreurs.c

--- TP2/src/erreurs.c
+++ TP2/src/erreurs.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    // Tableau de taille 100
-    int tableau[100];
-    // La boucle dépasse la taille réelle du tableau.
-    for (int compteur = 0; compteur < sizeof(tableau); compteur++) { // Erreur
-        tableau[compteur] = tableau[compteur] * 2;
-    }
 
-    return (0);
+   int tableau[100];
+
+   for (int compteur = 0; compteur < sizeof(tableau); compteur++) { //Erreur
+       tableau[compteur] = tableau[compteur] * 2;
+   }
+
+   return (0);
+
 }
+
