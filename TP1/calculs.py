38661e2c50ec63371a291d413661ffec819c1a63
Zizouwest <messaoud.louchene10@gmail.com>
2024-10-07 22:30:53 +0200

    Update calculs.py

--- TP1/calculs.py
+++ TP1/calculs.py
@@ -1,1 +1,28 @@
-op = input('Choississez votre
+# Demande à l'utilisateur d'entrer deux nombres et un opérateur
+num1 = int(input("Entrez le premier nombre : "))
+num2 = int(input("Entrez le deuxième nombre : "))
+op = input("Entrez l'opérateur (+, -, *, /, %, &, |, ~) : ")
+
+# Vérifie l'opérateur et effectue l'opération correspondante
+if op == '+':
+    result = num1 + num2
+elif op == '-':
+    result = num1 - num2
+elif op == '*':
+    result = num1 * num2
+elif op == '/':
+    result = num1 / num2
+elif op == '%':
+    result = num1 % num2
+elif op == '&':
+    result = num1 & num2
+elif op == '|':
+    result = num1 | num2
+elif op == '~':
+    result = ~num1  # Opérateur NOT prend seulement num1
+else:
+    result = "Opérateur invalide"
+
+# Affiche le résultat
+print(f"Le résultat de l'opération est : {result}")
+
