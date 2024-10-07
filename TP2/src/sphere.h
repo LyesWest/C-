c6d5c1a606c436196cadd9cc4748bdb8d123bc9c
LyesWest <lyes.mesbahi1@gmail.com>
2024-10-07 21:47:58 +0200

    Create sphere.h

---
+++ TP2/src/sphere.h
@@ -0,0 +1,10 @@
+//.h - Fichier d'en-tête contenant les déclarations de fonctions 
+
+// bibliothèque mathématique 
+#include <math.h>
+
+#define M_PI 3.14159265358979323846
+
+float calculer_surface(float rayon);
+
+float calculer_volume(float rayon);
