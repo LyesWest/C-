8d890ef1c083623ac16f67ad2a2e02f30d808c2f
LyesWest <lyes.mesbahi1@gmail.com>
2024-10-07 22:27:31 +0200

    Update main.c

--- TP2/src/main.c
+++ TP2/src/main.c
@@ -3,12 +3,11 @@
 
 int main() {
     float rayon = 5.0;
-    
-    float surface = calculer_surface(rayon);
-    printf("La surface de la sphère de rayon %.2f est %.2f\n", rayon, surface);
-
     float volume = calculer_volume(rayon);
+    float surface = calculer_surface(rayon);
+
     printf("Le volume de la sphère de rayon %.2f est %.2f\n", rayon, volume);
+    printf("La surface de la sphère de rayon %.2f est %.2f\n", rayon, surface);
 
     return 0;
 }
